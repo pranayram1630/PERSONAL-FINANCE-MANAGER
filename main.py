4a2f3bf446d12f71051725ed6f694ed41ef992f6
pranayram1630 <pranayramteke1630@gmail.com>
2024-09-09 16:45:37 +0530

    added fields

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import csv
 from datetime import datetime
+from data_entry import get_amount, get_category, get_date, get_description
 
 
 class CSV:
@@ -28,9 +29,19 @@ class CSV:
             writer.writerow(new_entry)
             print("Entry added successfully")
 
+def add():
+    CSV.initialize_csv()
+    date = get_date("Enter the date of the transaction (dd-mm-yyyy) or enter today's date: ", allow_default=True)
+    amount = get_amount()
+    category = get_category()
+    description = get_description()
+    CSV.add_entry(date, amount, category, description)
 
 
-CSV.initialize_csv()
-CSV.add_entry("20-07-2024", 125.65, "Income", "Salary")
+
+
+# CSV.initialize_csv()
+# CSV.add_entry("20-07-2024", 125.65, "Income", "Salary")
+add()
 
         
